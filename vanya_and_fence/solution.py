88cf5ae735429e29b43f92c99882baadd689e359
Denys Lins <denys@DESKTOP-MCAQ2GP.localdomain>
2020-01-28 22:05:10 -0300

    Add vanya_and_fence solution

--- vanya_and_fence/solution.py
+++ vanya_and_fence/solution.py
@@ -1,3 +1,12 @@
 n, h = input().split(" ")
 a = input().split(" ")
-print(a)
+
+result = 0
+
+for i in range(int(n)):
+  if int(a[i]) <= int(h):
+    result += 1
+  else:
+    result +=2
+
+print(result)
