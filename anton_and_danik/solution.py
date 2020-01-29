841b2927ae5a67c03588121ed9e80b97cdcacaac
Denys Lins <denyslins@gmail.com>
2020-01-28 23:10:32 -0300

    Add anton_and_danik solution

---
+++ anton_and_danik/solution.py
@@ -0,0 +1,17 @@
+n = input()
+s = input().strip()
+
+a, d = 0, 0
+
+for i in s:
+  if i == 'A':
+    a += 1
+  else:
+    d += 1
+
+if a > d:
+  print('Anton')
+elif d > a:
+  print('Danik')
+else:
+  print('Friendship')
